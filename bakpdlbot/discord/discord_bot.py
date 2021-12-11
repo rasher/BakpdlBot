a1a91af54d425dde4177ce29111c884420d3e835
Mick Boekhoff <mickboekhoff@hotmail.com>
2021-12-11 13:24:12 +0100

    002 Add zwift events url to bot commands.

--- bakpdlbot/discord/discord_bot.py
+++ bakpdlbot/discord/discord_bot.py
@@ -13,7 +13,17 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='sheet', help='shares the link to the google sheet')
-async def testing(ctx):
-    await ctx.send('Find the google sheet at:\nhttps://docs.google.com/spreadsheets/d/16ip9cd6kpH2fl0dJYlG4UC1VOJL2eYjd8WSSXJMjss4/edit?usp=sharing')
+async def sheet(ctx):
+    message='Find the google sheet at:\n' \
+            '<https://docs.google.com/spreadsheets/d/16ip9cd6kpH2fl0dJYlG4UC1VOJL2eYjd8WSSXJMjss4/edit?usp=sharing>'
+    await ctx.send(message)
+
+@bot.command(name='events', help='shares the link to backpedal zwift events')
+async def events(ctx):
+    message='Find our Backpedal events here:\n' \
+            '<https://www.zwift.com/events/tag/backpedal>\n' \
+            '<https://zwiftpower.com/series.php?id=BACKPEDAL>'
+    await ctx.send(message)
+
 
 bot.run(TOKEN)
