e69edd5dd0aaa9c48799dcb848384ba3eff1ea56
Jonas Häggqvist <rasher@rasher.dk>
2021-12-21 23:02:29 +0100

    Use cli script to launch bot, enable logging

--- bakpdlbot/cli.py
+++ bakpdlbot/cli.py
@@ -1,15 +1,23 @@
 """Console script for bakpdlbot."""
+import logging
+import os
 import sys
+
 import click
+from dotenv import load_dotenv
+
+from .discord_bot import bot
 
 
 @click.command()
-def main(args=None):
+@click.option('--debug', is_flag=True, help='Enable debug logging')
+def main(debug, args=None):
     """Console script for bakpdlbot."""
-    click.echo("Replace this message by putting your code into "
-               "bakpdlbot.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+    level = logging.DEBUG if debug else logging.INFO
+    logging.basicConfig(level=level)
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    bot.run(TOKEN)
 
 
 if __name__ == "__main__":
