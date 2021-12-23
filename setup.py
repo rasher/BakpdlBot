7fc0057c4a41439d94088290f2f3f89a0b8cfb51
Mick Boekhoff <mickboekhoff@hotmail.com>
2021-12-23 22:36:15 +0100

    update setup.

--- setup.py
+++ setup.py
@@ -24,13 +24,14 @@ test_requirements = [ ]
 setup(
     author="Mick Boekhoff",
     author_email='mickboekhoff@hotmail.com',
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
