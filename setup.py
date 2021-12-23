6d86339b8ce13ce80f5250678e458a632199b617
Jonas Häggqvist <rasher@rasher.dk>
2021-12-23 03:42:21 +0100

    Use compatible version in setup.py requirements

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-        'Click>=7.0',
-        'discord>=1.7.3,<2',
-        'python-dotenv>=0.19.2,<0.20',
-        'google-api-python-client>=2.33.0,<2.34',
-        'google-auth-oauthlib>=0.4.6,<0.5',
-        'tabulate>=0.8.9,<0.9',
+        'Click~=8.0.3',
+        'discord~=1.7.3',
+        'python-dotenv~=0.19.2',
+        'google-api-python-client~=2.33.0',
+        'google-auth-oauthlib~=0.4.6',
+        'tabulate~=0.8.9',
         'ago~=0.0.93',
         'requests~=2.26.0',
         'pendulum~=2.1.2',
