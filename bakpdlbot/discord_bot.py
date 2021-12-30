2cdf6175fdb61b2ab691119653bb79c6eb231886
Mick Boekhoff <mickboekhoff@hotmail.com>
2021-12-30 23:40:14 +0100

    add channels to respond zwift events to.

--- bakpdlbot/discord_bot.py
+++ bakpdlbot/discord_bot.py
@@ -63,7 +63,7 @@ async def event_embed(message, event):
 
 @bot.listen("on_message")
 async def zwift_link_embed(message):
-    if message.channel.name not in ('bot-test', 'looking-for-company', 'team-bus-event-talk'):
+    if message.channel.name not in ('bot-test', 'looking-for-company', 'team-bus-event-talk', 'bakpdl-festive-500', 'weekend-bunchy', 'backpedal-hc-series'):
         return
     eventlink = re.compile(r'https://www.zwift.com/.*events/.*view/(?P<eid>[0-9]+)(?:\?eventSecret=(?P<secret>[0-9a-z]+))?')
     m = eventlink.search(message.content)
